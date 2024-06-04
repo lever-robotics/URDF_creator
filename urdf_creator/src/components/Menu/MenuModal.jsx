29c88de664efc9628c40fc98fb8e5a1b57827cb7
Jared Smith <a02305009@usu.edu>
2024-06-04 15:39:46 -0600

    functioning styled menu button

--- urdf_creator/src/components/Menu/MenuModal.jsx
+++ urdf_creator/src/components/Menu/MenuModal.jsx
@@ -3,11 +3,47 @@ import { useState } from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { styled } from '@mui/material/styles';
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 import './MenuModal.css'
 
 export default function MenuModal() {
   const [projectTitle, setProjectTitle] = useState('untitled');
+
+  const StyledMenuItem = styled(props => (
+    <MenuItem {...props} disableRipple/>
+  ))(({ theme }) => ({
+    fontSize: "3em",
+    fontWeight: 200,
+    fontFamily: "inherit",
+    transition: 'font-size 0.4s',
+    '&:hover, &.Mui-focusVisible':{
+      fontSize: '4em',
+    },
+  }));
+
+  const StyledMenu = styled((props) => (
+    <Menu
+      {...props}
+    />
+  ))(({ theme }) => ({
+    '& .MuiPaper-root': {
+      background: 'transparent',
+      color: 'white',
+      boxShadow: 'none',
+      height: '100%',
+      width: '30%',
+    },
+    '& .MuiList-root': {
+      textOverflow: 'ellipsis',
+      height: '90%',
+      display: 'flex',
+      paddingLeft: '15%',
+      justifyContent: 'space-evenly',
+      flexDirection: 'column',
+    },
+    background:'rgba(0, 0, 0, 0.6)',
+  }));
 
   return (
     <PopupState variant="popover" popupId="demo-popup-menu">
@@ -19,11 +55,12 @@ export default function MenuModal() {
             </Button>
             <div Id='projectTitle'>{projectTitle}</div>
           </div>
-          <Menu {...bindMenu(popupState)}>
-            <MenuItem className='MenuItem' onClick={popupState.close}>Profile</MenuItem>
-            <MenuItem className='MenuItem' onClick={popupState.close}>My account</MenuItem>
-            <MenuItem className='MenuItem' onClick={popupState.close}>Logout</MenuItem>
-          </Menu>
+          <StyledMenu {...bindMenu(popupState)}>
+            <StyledMenuItem onClick={popupState.close}>Profile</StyledMenuItem>
+            <StyledMenuItem onClick={popupState.close}>My account</StyledMenuItem>
+            <StyledMenuItem onClick={popupState.close}>Logout</StyledMenuItem>
+            <StyledMenuItem onClick={popupState.close}>Another Button</StyledMenuItem>
+          </StyledMenu>
         </React.Fragment>
       )}
     </PopupState>
