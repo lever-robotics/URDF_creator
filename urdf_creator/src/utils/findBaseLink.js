0c59291925da092d3a682fa1d399c1102302cfa7
Kayson <a02303285@usu.edu>
2024-06-06 18:11:42 -0600

    fixed findBaseLink

--- urdf_creator/src/utils/findBaseLink.js
+++ urdf_creator/src/utils/findBaseLink.js
@@ -1,8 +1,10 @@
+import urdfObject from "../Models/urdfObject";
+
 export default function findBaseLink(scene) {
     if (scene) {
         if (scene.children) {
             const children = scene.children.filter((child) => {
-                return child.sceneObject;
+                return child instanceof urdfObject;
             });
             if (children.length > 0) {
                 return children[0];
