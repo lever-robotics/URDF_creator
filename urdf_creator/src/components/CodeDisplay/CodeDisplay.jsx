33fb5abf2fabbcdfcf77f05c4f58533dfb891478
Jared Smith <a02305009@usu.edu>
2024-07-19 17:45:25 -0600

    inital clean of codeDisplayer. Made it more React-like and added descriptive comments

--- urdf_creator/src/components/CodeDisplay/CodeDisplay.jsx
+++ urdf_creator/src/components/CodeDisplay/CodeDisplay.jsx
@@ -1,64 +1,129 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { ScenetoXML } from "../../utils/ScenetoXML";
-import { ScenetoSDF } from "../../utils/ScenetoSDF";
-import './CodeDisplay.css'; // Assuming you have a CSS file for this component
+import ScenetoText from "../../utils/ScenetoText";
+import "./CodeDisplay.css"; // Assuming you have a CSS file for this component
 
+/**
+ * Displays buttons to determine with format the text representation of the scene should be displayed in as well as a text box containing the text
+ * @param {Scene} scene The scene object
+ * @param {string} projectTitle  
+ * @returns Format buttons and a text box
+ */
 export default function CodeDisplay({ scene, projectTitle }) {
-    const [format, setFormat] = useState('urdf');
-    const [tooltip, setTooltip] = useState({ show: false, x: 0, y: 0 });
+    const [selectedFormat, setSelectedFormat] = useState("URDF");
 
-    const copyToClipboard = (text, e) => {
-        navigator.clipboard.writeText(text).then(() => {
-            const { clientX: x, clientY: y } = e; // Get mouse position
-            setTooltip({ show: true, x, y });
-            setTimeout(() => setTooltip({ show: false, x: 0, y: 0 }), 1000); // Hide tooltip after 2 seconds
-        }, (err) => {
-            console.error('Could not copy text: ', err);
-        });
-    };
-
-    const renderContent = () => {
-        const style = { fontSize: "12px", overflow: "auto", maxHeight: "50%", flexGrow: 1 };
-        let code;
-
-        switch (format) {
-            case 'urdf':
-                code = ScenetoXML(scene, projectTitle);
-                break;
-            case 'sdf':
-                code = ScenetoSDF(scene, projectTitle);
-                break;
-            case 'xacro':
-                code = "Xacro not currently supported. This format is for programmatically writing urdf files.";
-                break;
-            default:
-                code = "";
-        }
-
-        return (
-            <div onClick={(e) => copyToClipboard(code, e)} className="code-container">
-                <SyntaxHighlighter language={format === 'xacro' ? 'text' : 'xml'} style={atomDark} customStyle={style}>
-                    {code}
-                </SyntaxHighlighter>
-                {tooltip.show && (
-                    <div className="tooltip" style={{ left: tooltip.x, top: tooltip.y }}>
-                        Copied!
-                    </div>
-                )}
-            </div>
-        );
+    const handleClick = (e) => {
+        const format = e.target.innerText;
+        setSelectedFormat(format);
     };
 
     return (
-        <div style={{ display: "flex", flexDirection: "column", maxHeight: "100%", flexGrow: 1, flexBasis: 0 }}>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                maxHeight: "100%",
+                flexGrow: 1,
+                flexBasis: 0,
+            }}>
             <div>
-                <button className={format === 'urdf' ? 'button_selected' : ''} onClick={() => setFormat('urdf')}>URDF</button>
-                <button className={format === 'sdf' ? 'button_selected' : ''} onClick={() => setFormat('sdf')}>SDF</button>
-                <button className={format === 'xacro' ? 'button_selected' : ''} onClick={() => setFormat('xacro')}>XACRO</button>
+                <button
+                    className={
+                        selectedFormat === "URDF" ? "button_selected" : ""
+                    }
+                    onClick={handleClick}>
+                    URDF
+                </button>
+                <button
+                    className={
+                        selectedFormat === "SDF" ? "button_selected" : ""
+                    }
+                    onClick={handleClick}>
+                    SDF
+                </button>
+                <button
+                    className={
+                        selectedFormat === "XACRO" ? "button_selected" : ""
+                    }
+                    onClick={handleClick}>
+                    XACRO
+                </button>
             </div>
-            {renderContent()}
+            <CodeBox
+                scene={scene}
+                projectTitle={projectTitle}
+                selectedFormat={selectedFormat}
+            />
+        </div>
+    );
+}
+
+/**
+ * Contains the text representation of the scene object
+ * @param {Scene} scene The scene object
+ * @param {string} projectTitle
+ * @param {string} selectedFormat The format the text representation of the scene will be displayed in
+ * @returns A text box with syntax highlighting and text representation of the scene
+ */
+function CodeBox({ scene, projectTitle, selectedFormat }) {
+    const style = {
+        fontSize: "12px",
+        overflow: "auto",
+        maxHeight: "50%",
+        flexGrow: 1,
+    };
+    const [copied, setCopied] = useState(false);
+    const [mousePosition, setMousePosition] = useState({});
+    const [code, setCode] = useState();
+
+    useEffect(() => {
+        const text = ScenetoText(selectedFormat, scene, projectTitle);
+        setCode(text);
+    }, [scene, selectedFormat]);
+
+    // Copies the text to the clipboard and displays a tooltip saying copied for two seconds
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(code).then(
+            () => {
+                setCopied(true);
+                setTimeout(() => setCopied(false), 1000); // Hide tooltip after 2 seconds
+            },
+            (err) => {
+                console.error("Could not copy text: ", err);
+            }
+        );
+    };
+
+    const handleClick = (e) => {
+        setMousePosition({ x: e.clientX, y: e.clientY });
+        copyToClipboard();
+    };
+
+    return (
+        <div onClick={handleClick} className="code-container">
+            <SyntaxHighlighter
+                language={selectedFormat === "XACRO" ? "text" : "xml"}
+                style={atomDark}
+                customStyle={style}>
+                {code}
+            </SyntaxHighlighter>
+            {copied && <Tooltip mousePosition={mousePosition} />}
+        </div>
+    );
+}
+
+/**
+ * A tooltip meant to display below the mouse when the mouse clicks on the codebox
+ * @param {*} mousePosition The x and y coords for the mouse
+ * @returns JSX component that displays the text 'Copied!'
+ */
+function Tooltip({ mousePosition }) {
+    return (
+        <div
+            className="tooltip"
+            style={{ left: mousePosition.x, top: mousePosition.y }}>
+            Copied!
         </div>
     );
 }
---
+++ urdf_creator/src/utils/ScenetoText.js
@@ -0,0 +1,27 @@
+import { ScenetoSDF } from "./ScenetoSDF";
+import { ScenetoXML } from "./ScenetoXML";
+import { ScenetoJSON } from "./ScenetoJSON";
+
+/**
+ * Returns the scene in text form in the specified format
+ * @param {string} format 
+ * @param {Scene} scene 
+ * @param {string} projectTitle 
+ * @returns Text representation of the scene object in desired format
+ */
+export default function ScenetoText(format, scene, projectTitle) {
+    switch(format){
+        case 'URDF': 
+            return ScenetoXML(scene, projectTitle);
+        case "XML":
+            return ScenetoXML(scene, projectTitle);
+        case 'SDF':
+            return ScenetoSDF(scene, projectTitle);
+        case "XACRO":
+            return "Xacro not currently supported. This format is for programmatically writing urdf files.";
+        case 'JSON':
+            return ScenetoJSON(scene, projectTitle);
+        default:
+            return "Error in ScenetoText";
+    }
+}
