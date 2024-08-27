9d91f7f95f046c6db2720a7e9b79a8756629d3dd
Jared Smith <a02305009@usu.edu>
2024-08-27 10:10:29 -0600

    Rotation Parameters updated

--- urdf_creator/src/components/ObjectParameters/Parameters/RotationParameters.js
+++ urdf_creator/src/components/ObjectParameters/Parameters/RotationParameters.js
@@ -1,22 +1,61 @@
+import { useState } from "react";
 import ToggleSection from "../ToggleSection";
 import Parameter from "./Parameter";
 
 function RotationParameters({ selectedObject, stateFunctions }) {
-
     const radToDeg = (radians) => (radians * 180) / Math.PI;
     const degToRad = (degrees) => (degrees * Math.PI) / 180;
 
+    const [tempX, setTempX] = useState(
+        radToDeg(selectedObject.rotation.x).toFixed(2)
+    );
+    const [tempY, setTempY] = useState(
+        radToDeg(selectedObject.rotation.y).toFixed(2)
+    );
+    const [tempZ, setTempZ] = useState(
+        radToDeg(selectedObject.rotation.z).toFixed(2)
+    );
+
     const handleRotationChange = (e) => {
+        const axis = e.target.title.toLowerCase().replace(":", "");
+        const tempValue = e.target.value;
+        switch (axis) {
+            case "x":
+                setTempX(tempValue);
+                break;
+            case "y":
+                setTempY(tempValue);
+                break;
+            case "z":
+                setTempZ(tempValue);
+                break;
+        }
+    };
+
+    const handleRotationBlur = (e) => {
         const axis = e.target.title.toLowerCase().replace(":", "");
         const newValue = parseFloat(e.target.value);
         if (isNaN(newValue)) return;
-
         stateFunctions.transformObject(
             selectedObject,
             "rotation",
             axis,
             degToRad(newValue)
         );
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            const axis = e.target.title.toLowerCase().replace(":", "");
+            const newValue = parseFloat(e.target.value);
+            if (isNaN(newValue)) return;
+            stateFunctions.transformObject(
+                selectedObject,
+                "rotation",
+                axis,
+                degToRad(newValue)
+            );
+        }
     };
 
     return (
@@ -26,22 +65,28 @@ function RotationParameters({ selectedObject, stateFunctions }) {
                     title="X:"
                     type="number"
                     units="°degrees"
-                    value={radToDeg(selectedObject.rotation.x).toFixed(2)}
+                    value={tempX}
                     onChange={handleRotationChange}
+                    onBlur={handleRotationBlur}
+                    onKeyDown={handleKeyDown}
                 />
                 <Parameter
                     title="Y:"
                     type="number"
                     units="°degrees"
-                    value={radToDeg(selectedObject.rotation.y).toFixed(2)}
+                    value={tempY}
                     onChange={handleRotationChange}
+                    onBlur={handleRotationBlur}
+                    onKeyDown={handleKeyDown}
                 />
                 <Parameter
                     title="Z:"
                     type="number"
                     units="°degrees"
-                    value={radToDeg(selectedObject.rotation.z).toFixed(2)}
+                    value={tempZ}
                     onChange={handleRotationChange}
+                    onBlur={handleRotationBlur}
+                    onKeyDown={handleKeyDown}
                 />
             </ul>
         </ToggleSection>
