acebcc330267ab541dd193212a6cc26d2f1f6034
Kayson Argyle <kaysonargyle@gmail.com>
2024-09-02 20:24:57 -0600

    changed to use force update code when reparenting

--- urdf_creator/src/components/SceneState.jsx
+++ urdf_creator/src/components/SceneState.jsx
@@ -402,7 +402,7 @@ export default function SceneState({ threeScene }) {
     const reparentObject = (parent, child) => {
         parent.link.attach(child);
         child.parentURDF = parent;
-        forceSceneUpdate();
+        forceUpdateCode();
     };
 
     const stateFunctions = {
