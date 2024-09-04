c054973e005baab83d63c24381b469618703009b
Mark Soulier <mark.soulier@usu.edu>
2024-09-04 09:57:25 -0600

    add resources to dev

--- urdf_creator/src/components/ProjectManager/ProjectDisplayer.jsx
+++ urdf_creator/src/components/ProjectManager/ProjectDisplayer.jsx
@@ -6,7 +6,8 @@ const ProjectDisplayer = ({ handleProjectClick, onClose }) => {
   const [selectedProject, setSelectedProject] = useState(null);
   const [projects, setProjects] = useState([
       { title: 'New Project', description: 'Create a new URDF Project', image:'/statics/PlusIcon.jpg', width: '200', height: '200' },
-      { title: 'Turtlebot3 Burger', description: 'Turtlebot Designed by ROBOTIS', path: '/statics/turtlebot3_burger.gltf', image: '/statics/turtlebot3_burger.png', width: '200', height: '200' },
+      { title: 'Turtlebot3 Burger', description: 'Turtlebot Designed by ROBOTIS', path: '/statics/turtlebot3_burger.gltf', image: '/statics/turtlebot3_burger.png', width: '100', height: '200' },
+      { title: 'Turtlebot3 Waffle', description: 'Turtlebot Designed by ROBOTIS', path: '/statics/turtlebot3_burger.gltf', image: '/statics/turtlebot3_waffle.png', width: '100', height: '200' },
       { title: 'Fun Robot', description: 'Test out the functionality of ROS2', path: '/statics/green_robot.gltf', image: '/statics/green_robot.png', width: '200', height: '200' },
       { title: 'OldMain', description: 'Haha we go to college here', image:'/statics/oldmain.jpg', width: '200', height: '200' },
       // Add more projects
